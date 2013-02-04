080383e3107f74b5d3c410e521512fca14cdb70a
Matan Nassau <matan.nassau@gmail.com>
2013-02-03 20:16:33 -0500

    extract pushd_temporary_directory.tcc

--- pushd_temporary_directory.hh
+++ pushd_temporary_directory.hh
@@ -2,47 +2,25 @@
 #define FIXTURES_PUSHD_TEMPORARY_DIRECTORY_HH_
 
 #include <boost/filesystem/path.hpp>
-#include <boost/filesystem/operations.hpp>
-#include <boost/system/error_code.hpp>
 #include "temporary_directory.hh"
 #include <string>
 
 namespace fixtures {
 struct pushd_temporary_directory {
-    pushd_temporary_directory(std::string const& name)
-    : temp_dir(name)
-    , original_path(boost::filesystem::current_path())
-    {
-        boost::filesystem::current_path(path());
-    }
-
-    pushd_temporary_directory()
-    : temp_dir()
-    , original_path(boost::filesystem::current_path())
-    {
-        boost::filesystem::current_path(path());
-    }
-
-    ~pushd_temporary_directory()
-    {
-        boost::system::error_code err;
-        boost::filesystem::current_path(original_path, err);
-        if( err ) {
-            std::cerr
-                << "WARNING:failed to change current directory back to "
-                << original_path.string() << '\n';
-        }
-    }
-
+    pushd_temporary_directory(std::string const& name);
+    pushd_temporary_directory();
+    ~pushd_temporary_directory();
     pushd_temporary_directory(pushd_temporary_directory const&) = delete;
     pushd_temporary_directory& operator=(pushd_temporary_directory const&) = delete;
 
-    const boost::filesystem::path& path() const { return temp_dir.path(); }
+    const boost::filesystem::path& path() const;
 
 private:
     temporary_directory temp_dir;
     boost::filesystem::path original_path;
 };
 }  // namespace fixtures
+
+#include "pushd_temporary_directory.tcc"
 
 #endif  // FIXTURES_PUSHD_TEMPORARY_DIRECTORY_HH_
---
+++ pushd_temporary_directory.tcc
@@ -0,0 +1,41 @@
+#ifndef FIXTURES_PUSHD_TEMPORARY_DIRECTORY_TCC_
+#define FIXTURES_PUSHD_TEMPORARY_DIRECTORY_TCC_
+
+#include <string>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/system/error_code.hpp>
+
+namespace fixtures {
+pushd_temporary_directory::pushd_temporary_directory(std::string const& name)
+: temp_dir(name)
+, original_path(boost::filesystem::current_path())
+{
+    boost::filesystem::current_path(path());
+}
+
+pushd_temporary_directory::pushd_temporary_directory()
+: temp_dir()
+, original_path(boost::filesystem::current_path())
+{
+    boost::filesystem::current_path(path());
+}
+
+pushd_temporary_directory::~pushd_temporary_directory()
+{
+    boost::system::error_code err;
+    boost::filesystem::current_path(original_path, err);
+    if( err ) {
+        std::cerr
+            << "WARNING:failed to change current directory back to "
+            << original_path.string() << '\n';
+    }
+}
+
+const boost::filesystem::path& path() const
+{
+    return temp_dir.path();
+}
+}  // namespace fixtures
+
+#endif  // FIXTURES_PUSHD_TEMPORARY_DIRECTORY_TCC_
