abeaa4aedd5c2902679d7cd870f07fee6b57bf70
Matan Nassau <matan.nassau@gmail.com>
2013-11-06 06:41:20 -0500

    temporary_file move semantics

--- temporary_file.hh
+++ temporary_file.hh
@@ -7,9 +7,11 @@ namespace fixtures {
 struct temporary_file {
     explicit temporary_file(boost::filesystem::path p);
     temporary_file();
+    temporary_file(temporary_file&& rhs);
     ~temporary_file();
     temporary_file(temporary_file const&) = delete;
     temporary_file& operator=(temporary_file const&) = delete;
+    temporary_file& operator=(temporary_file&& rhs);
 
     boost::filesystem::path path() const;
 
